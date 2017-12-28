d6558eba4548781096ade1032148827d1f0aa575
gaps013 <gaps013@users.noreply.github.com>
2017-12-28 18:57:55 +0530

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,15 +1,20 @@
 import random
-def binary_search(input_list,start,end,number):
+def binary_search(input_array,start,end,number):
   middle = start+(end-start)//2
   if(end>=start):
-    if(number == input_list[middle]):
-      return input_list[middle]
-    elif(number>input_list[middle]):
-      return binary_search(input_list,middle+1,end,number)
-    elif(number<input_list[middle]):
-      return binary_search(input_list,start,middle-1,number)
+    if(number == input_array[middle]):
+      return middle
+    elif(number>input_array[middle]):
+      return binary_search(input_array,middle+1,end,number)
+    elif(number<input_array[middle]):
+      return binary_search(input_array,start,middle-1,number)
   else:
     return -1
-input_list = random.sample(range(1, 10000), 100)
-input_list.sort()
-print(binary_search(input_list,0,99,input_list[91]))
+input_array = random.sample(range(1, 10000), 100)
+input_array.sort()
+key= 9981
+index_position = binary_search(input_array,0,len(input_array)-1,key)
+if(index_position!=-1):
+  print("Element found at index position ",index_position)
+else:
+  print("Element not found")
